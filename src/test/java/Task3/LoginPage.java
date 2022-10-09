83150a8eb5644b9aa8b6a39dfd7279d6e8e843f8
Maaloks <maaloks91@gmail.com>
2022-10-09 14:41:48 +0300

    Task3 css and xpath locators

--- src/test/java/Task3/LoginPage.java
+++ src/test/java/Task3/LoginPage.java
@@ -5,16 +5,16 @@ package Task3;
 
 public class LoginPage {
     String loginUrl = "https://butlers.ua/ua/login?returnUrl=%2Fua%2F";
-    String regButtonXpath = "//div[contains(@class, 'login-page')]//input[@type=\"button\"][@value=\"Зареєструватися\"]";
-    String regButtonCss = "div [class*=\"login-page\"] input[type=\"button\"][value=\"Зареєструватися\"]";
-    String emailFieldXpath = "//div[contains(@class, 'login-page')]//input[@class=\"email\"]";
-    String emailFieldCss = "div [class*=\"login-page\"] #Email";
-    String passFieldXpath = "//div[contains(@class, 'login-page')]//input[@class=\"password\"]";
-    String passFieldCss = "div [class*=\"login-page\"] #Password";
-    String chkRememberMeXpath = "//div[contains(@class, 'login-page')]//input[@type=\"checkbox\"]";
-    String chkRememberMeCss = "div [class*=\"login-page\"] #RememberMe";
-    String passRecoveryXpath = "//div[contains(@class, 'login-page')]//a[contains(@href, 'passwordrec')]";
+    String regButtonXpath = "//div[contains(@class,'login-page')]//input[@type=\"button\"][@value=\"Зареєструватися\"]";
+    String regButtonCss = "div[class*=\"login-page\"] input[type=\"button\"][value=\"Зареєструватися\"]";
+    String emailFieldXpath = "//div[contains(@class,'login-page')]//input[@class=\"email\"]";
+    String emailFieldCss = "div[class*=\"login-page\"] #Email";
+    String passFieldXpath = "//div[contains(@class,'login-page')]//input[@class=\"password\"]";
+    String passFieldCss = "div[class*=\"login-page\"] #Password";
+    String chkRememberMeXpath = "//div[contains(@class,'login-page')]//input[@type=\"checkbox\"]";
+    String chkRememberMeCss = "div[class*=\"login-page\"] #RememberMe";
+    String passRecoveryXpath = "//div[contains(@class,'login-page')]//a[contains(@href,'passwordrec')]";
     String passRecoveryCss = "div[class*=\"login-page\"] a[href*='passwordrec']";
-    String linkLoginXpath = "(//a)[2]";
+    String linkLoginXpath = "//div[@class='header-links']//a[contains(text(),'Вхід')]"; //Not good idea to use index in xpath. Try to build another locator (with text).
     String linkLoginCss = "div.header-links a[href*='login']";
 }
--- src/test/java/Task3/ProfilePage.java
+++ src/test/java/Task3/ProfilePage.java
@@ -5,7 +5,7 @@ package Task3;
 
 
 public class ProfilePage {
-    String linkAccountXpath = "(//a)[2]";
+    String linkAccountXpath = "//div[@class='header-links']//a[contains(text(),'Персональний кабінет')]";
     String linkAccountCss = "div.header-links a[href*='customer']";
     String butNavInfoAcXpath = "//div[contains(@class,'account-navigation')]//a[contains(@href,'info')]";
     String butNavInfoAcCss = "div [class*='account-navigation'] a[href*='info']";
@@ -21,28 +21,28 @@ public class ProfilePage {
     String butNavReviewsAcCss = "div [class*='account-navigation'] a[href*='reviews']";
     String butNavLogOutAcXpath = "//div[contains(@class,'account-navigation')]//a[contains(@href,'logout')]";
     String butNavLogOutAcCss = "div [class*='account-navigation'] a[href*='logout']";
-    String radioMGenderXpath = "//div[contains(@class, 'account-page')]//input[@type='radio' and @value='M']";
+    String radioMGenderXpath = "//div[contains(@class,'account-page')]//input[@type='radio' and @value='M']";
     String radioMGenderCss = "div [class*='account-page'] input[type=radio][value='M']";
-    String radioFGenderXpath = "//div[contains(@class, 'account-page')]//input[@type='radio' and @value='F']";
+    String radioFGenderXpath = "//div[contains(@class,'account-page')]//input[@type='radio' and @value='F']";
     String radioFGenderCss = "div [class*='account-page'] input[type=radio][value='F']";
-    String fieldFNameXpath = "//div[contains(@class, 'account-page')]//input[@name='FirstName']";
-    String fieldLNameXpath = "//div[contains(@class, 'account-page')]//input[@name='LastName']";
-    String fieldFNameCss = "div [class*='account-page'] #FirstName";
-    String fieldLNameCss = "div [class*='account-page'] #LastName";
-    String optionDateOfBirthXpath = "//div[contains(@class, 'account-page')] //select[contains(@name,'Day')]";
-    String optionDateOfBirthCss = "div [class*='account-page'] select[name*='Day']";
-    String optionMonthOfBirthXpath = "//div[contains(@class, 'account-page')] //select[contains(@name,'Month')]";
-    String optionMonthOfBirthCss = "div [class*='account-page'] select[name*='Month']";
-    String optionYearOfBirthXpath = "//div[contains(@class, 'account-page')] //select[contains(@name,'Year')]";
-    String optionYearOfBirthCss = "div [class*='account-page'] select[name*='Year']";
-    String fieldEmailXpath = "//div[contains(@class, 'account-page')]//input[@name='Email']";
-    String fieldEmailCss = "div [class*='account-page'] #Email";
-    String fieldPhoneXpath = "//div[contains(@class, 'account-page')]//input[@name='Phone']";
-    String fieldPhoneCss = "div [class*='account-page'] #Phone";
+    String fieldFNameXpath = "//div[contains(@class,'account-page')]//input[@name='FirstName']";
+    String fieldLNameXpath = "//div[contains(@class,'account-page')]//input[@name='LastName']";
+    String fieldFNameCss = "div[class*='account-page'] #FirstName";
+    String fieldLNameCss = "div[class*='account-page'] #LastName";
+    String optionDateOfBirthXpath = "//div[contains(@class,'account-page')]//select[contains(@name,'Day')]";
+    String optionDateOfBirthCss = "div[class*='account-page'] select[name*='Day']";
+    String optionMonthOfBirthXpath = "//div[contains(@class,'account-page')]//select[contains(@name,'Month')]";
+    String optionMonthOfBirthCss = "div[class*='account-page'] select[name*='Month']";
+    String optionYearOfBirthXpath = "//div[contains(@class,'account-page')]//select[contains(@name,'Year')]";
+    String optionYearOfBirthCss = "div[class*='account-page'] select[name*='Year']";
+    String fieldEmailXpath = "//div[contains(@class,'account-page')]//input[@name='Email']";
+    String fieldEmailCss = "div[class*='account-page'] #Email";
+    String fieldPhoneXpath = "//div[contains(@class,'account-page')]//input[@name='Phone']";
+    String fieldPhoneCss = "div[class*='account-page'] #Phone";
     String chkNewsXpath = "//div[contains(@class,'account-page')]//input[@type='checkbox' and starts-with(@name,'News')]";
-    String chkNewsCss = "div [class*='account-page'] input[type=checkbox][id^='News']";
-    String LinkloginXpath = "//div[contains(@class, 'account-page')]//a[last()]";
-    String LinkLoginCss = "div [class*='account-page'] a:last-of-type";
-    String buttonSaveXpath = "//div[contains(@class, 'account-page')]//input[starts-with(@name,'save-info')]";
-    String buttonSaveCss = "div [class*='account-page'] input[id^='save-info']";
+    String chkNewsCss = "div[class*='account-page'] input[type=checkbox][id^='News']";
+    String LinkloginXpath = "//div[contains(@class,'account-page')]//a[last()]";
+    String LinkLoginCss = "div[class*='account-page'] a:last-of-type";
+    String buttonSaveXpath = "//div[contains(@class,'account-page')]//input[starts-with(@name,'save-info')]";
+    String buttonSaveCss = "div[class*='account-page'] input[id^='save-info']";
 }
